4618cf3045828c6e2720d22b70b8cf54b9b26271
programmerpratik97 <pratikghodke1997@gmail.com>
2025-09-21 16:04:06 +0530

    added book model

---
+++ src/model/Book.java
@@ -0,0 +1,54 @@
+package src.model;
+
+public class Book {
+    private final String isbn;
+    private String title;
+    private String author;
+    private int publicationYear;
+    private boolean isAvailable;
+
+    public Book(String isbn, String title, String author, int publicationYear) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isAvailable = true;
+    }
+
+    public String getIsbn()
+    {
+        return isbn;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public void setPublicationYear(int year) {
+        this.publicationYear = year;
+    }
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Book[%s: %s by %s, %d]", isbn, title, author, publicationYear);
+    }
+}
+
+}
